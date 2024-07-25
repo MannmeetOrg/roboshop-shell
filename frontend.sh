af6f9622e1d5a7dd31872bf51a48b19005e3c4a2
Mannmeet <manmeet.a.jb@gmail.com>
2024-07-25 18:53:58 +0530

    Configuration committed

--- frontend.sh
+++ frontend.sh
@@ -1,7 +1,7 @@
 source common.sh
 
 PRINT Disable NGINX
-dnf module disable nginx -y
+dnf module disable nginx -y &>>$LOG_FILE
 
 PRINT Disable NGINX
 dnf module enable nginx:1.24 -y &>>$LOG_FILE
@@ -9,25 +9,32 @@ STAT $?
 
 PRINT Install NGINX
 dnf install nginx -y &>>$LOG_FILE
+STAT $?
 
 PRINT Start & Enable Nginx service
 
 systemctl enable nginx &>>$LOG_FILE
 systemctl start nginx &>>$LOG_FILE
+STAT $?
 
 PRINT Remove the default content that web server is serving.
 
 rm -rf /usr/share/nginx/html/* &>>$LOG_FILE
+STAT $?
 
 PRINT Download the frontend content
 
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend-v3.zip &>>$LOG_FILE
+STAT $?
 
 PRINT Extract the frontend content.
 
 cd /usr/share/nginx/html &>>$LOG_FILE
 unzip /tmp/frontend.zip &>>$LOG_FILE
+STAT $?
 
 PRINT Restart Nginx Service to load the changes of the configuration.
 
 systemctl restart nginx &>>$LOG_FILE
+
+STAT $?
