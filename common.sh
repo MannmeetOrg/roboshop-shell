5a066d72c2e554890a68910a6285063af5c76c01
Mannmeet <manmeet.a.jb@gmail.com>
2024-07-25 18:35:45 +0530

    Configuration committed

--- common.sh
+++ common.sh
@@ -9,3 +9,14 @@ PRINT () {
     echo " ####################################### $* ########################################" &>>$LOG_FILE
     echo $*
 }
+
+STAT() {
+  if [ $1 -eq 0 ]; then
+    echo -e "\e[32mSUCCESS\e[0m"
+  else
+    echo -e "\e[31mFAILURE\e[0m"
+    echo
+    echo "Refer the log file for more information : File Path : ${LOG_FILE}"
+    exit $1
+  fi
+}
