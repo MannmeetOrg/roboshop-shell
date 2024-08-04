e75605bb5445c08bfbdd20680b905c2cead45dcc
Mannmeet <manmeet.a.jb@gmail.com>
2024-08-04 21:19:46 +0530

    Configuration committed

--- mysql.sh
+++ mysql.sh
@@ -3,17 +3,17 @@ sname=mysql
 
 PRINT Install MySQL Server
 
-dnf install mysql-server -y
+dnf install mysql-server -y &>>$LOG_FILE
 STAT $?
 
 PRINT Start and Enable MySQL Service
 
-systemctl enable mysqld
-systemctl start mysqld
+systemctl enable mysqld &>>$LOG_FILE
+systemctl start mysqld &>>$LOG_FILE
 STAT $?
 
 PRINT Change root password to RoboShop@1
-mysql_secure_installation --set-root-pass RoboShop@1
+mysql_secure_installation --set-root-pass RoboShop@1 &>>$LOG_FILE
 STAT $?
 
 
