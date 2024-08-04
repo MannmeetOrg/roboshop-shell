c0ea12e78b6648f1a9119380b309dc6fdd98c125
Mannmeet <manmeet.a.jb@gmail.com>
2024-08-04 22:44:40 +0530

    Configuration committed

--- payment.sh
+++ payment.sh
@@ -1,6 +1,7 @@
 source common.sh
 component=payment
 sname=payment
+app_path=/app
 
 PRINT Install Python 3
 
@@ -9,7 +10,8 @@ dnf install python3 gcc python3-devel -y &>>$LOG_FILE
 Prereq_App
 
 PRINT Download Dependencies
-cd /app
-pip3 install -r requirements.txt
+cd /app &>>$LOG_FILE
+pip3 install -r requirements.txt &>>$LOG_FILE
+
 Systemd_setup
 execute_as_root
