7253f607a5ad7b37547776eaf1ef19a2b3deea5e
Mannmeet <manmeet.a.jb@gmail.com>
2024-08-04 20:54:12 +0530

    Configuration committed

--- redis.sh
+++ redis.sh
@@ -3,22 +3,22 @@ component=redis
 sname=redis
 
 PRINT  Disable redis
-dnf module disable redis -y
+dnf module disable redis -y &>>$LOG_FILE
 STAT $?
 
 PRINT  Enable  redis
-dnf module enable redis:7 -y
+dnf module enable redis:7 -y &>>$LOG_FILE
 STAT $?
 
 PRINT  Install redis
 
-dnf install redis -y
+dnf install redis -y &>>$LOG_FILE
 STAT $?
 
 PRINT  Update Redis Configuration
 
 #sed -i 's/127.0.0.0/0.0.0.0' /etc/redis/redis.conf
-sed -i -e '/^bind/ s/127.0.0.1/0.0.0.0/' -e '/protected-mode/ c protected-mode no' /etc/redis/redis.conf
+sed -i -e '/^bind/ s/127.0.0.1/0.0.0.0/' -e '/protected-mode/ c protected-mode no' /etc/redis/redis.conf &>>$LOG_FILE
 
 STAT $?
 
